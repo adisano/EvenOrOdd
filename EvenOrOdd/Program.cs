d0dedc755ff0c246a9c9b0353580d05a90e9a63c
Adriana Disano <adrianadisano@gmail.com>
2018-07-23 02:04:29 -0500

    Created

--- EvenOrOdd/Program.cs
+++ EvenOrOdd/Program.cs
@@ -6,7 +6,22 @@ namespace EvenOrOdd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Acquire user input
+            Console.WriteLine("Enter a number:");
+            string inputNumberStr = Console.ReadLine();
+
+            //convert user input from string to integer
+            int inputNumberInt = int.Parse(inputNumberStr);
+
+            //Check the remainder of the integer
+            int remainNumberInt = inputNumberInt % 2;
+
+            //Assign a value to the string
+            string evenOdd = (remainNumberInt == 0) ? "Even" : "Odd";
+
+            //Output result
+            Console.WriteLine("The number you entered was {0}", evenOdd);
+            Console.ReadLine();
         }
     }
 }
